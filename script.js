7b3ead9084c2c348009ab81f1f5dfde6f9bb0d7b
Riboww <ledaingoc2007@gmail.com>
2024-11-04 10:10:27 +0700

    First Website
    ok

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cartButtons = document.querySelectorAll('.add-to-cart');
+    cartButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Item added to cart!');
+        });
+    });
+});
